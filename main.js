a8da4c32183c5850cc05b14f77ab1166f1858ead
Abdalla-Shehata <bedoalexawy@gmail.com>
2023-07-30 18:43:33 +0200

    second commit

--- main.js
+++ main.js
@@ -1,58 +1,89 @@
-async function search(a) {
-    let t = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=7d77b96c972b4d119a3151101212704&q=${a}&days=3`);
-    if (t.ok && 400 != t.status) {
-        let a = await t.json();
-        displayCurrent(a.location, a.current), displayAnother(a.forecast.forecastday)
-    }
+var weatherData =[];
+async function weather (city) {
+    var apiResult = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=7d77b96c972b4d119a3151101212704&q=${city}&days=3`);
+    var finalResponse = await apiResult.json();
+        weatherData = finalResponse;
+    display ()
+    // console.log(weatherData);
+    console.log(weatherData.forecast.forecastday[0].date.split('-')[2] + monthName());
 }
-document.getElementById("search").addEventListener("keyup", a => { search(a.target.value) });
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+weather (`cairo`);
+document.getElementById("search").addEventListener("keyup", city => { weather(city.target.value) });
+// document.getElementById("search").addEventListener("keyup", function (city) {
+//     weather(city.target.value)
+// });
 
-function displayCurrent(a, t) {
-    if (null != t) {
-        var e = new Date(t.last_updated.replace(" ", "T"));
-        let n = `
-        <div class="today forecast">
+
+
+tRow = document.getElementById('tRow');
+
+function monthName() {
+    const months= ["January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"];
+    const d = new Date();
+    let month = months[d.getMonth()];
+    // console.log(name);
+    return month;
+}
+function dayName(param) {
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const d = new Date(param);
+    let day = days[d.getDay()];
+    // console.log(day);
+    return day;
+}
+
+
+function display () {
+    let box = `
+    <div class="col-lg-4">
+        <div class="items shadow p-2">
 
             <div class="forecast-header"  id="today">
-                <div class="day">${days[e.getDay()]}</div>
-                <div class=" date">${e.getDate() + monthNames[e.getMonth()]}</div>
+                <div><span>${dayName(weatherData.forecast.forecastday[0].date)}</span></div>
+                <div class=" date">${(weatherData.forecast.forecastday[0].date.split('-')[2] + monthName())}</div> 
             </div>
-                    \x3c!-- .forecast-header --\x3e
-            <div class="forecast-content" id="current">
-                <div class="location">${a.name}</div>
-                <div class="degree">
-                    <div class="num">${t.temp_c}<sup>o</sup>C</div>
-                    <div class="forecast-icon">
-                        <img src="https:${t.condition.icon}" alt="" width=90>
-                    </div>
-                </div>
-                <div class="custom">${t.condition.text}</div>
-                <span><img src="images/icon-umberella.png" alt="">20%</span>
-                <span><img src="images/icon-wind.png" alt="">18km/h</span>
-                <span><img src="images/icon-compass.png" alt="">East</span>
-            </div>
-        </div>`; 
-        document.getElementById("forecast").innerHTML = n
-    }
-}
-function displayAnother(a) {
-    let t = ""; for (let e = 1; e < a.length; e++)t +=
-     `\t<div class="forecast">
-         <div class="forecast-header">    
-          <div class="day">${days[new Date(a[e].date.replace(" ", "T")).getDay()]}</div>
-            </div> \x3c!-- .forecast-header --\x3e 
-             <div class="forecast-content">
-                <div class="forecast-icon">
-                  <img src="https:${a[e].day.condition.icon}" alt="" width=48>
-                    </div> 
-                    <div class="degree">${a[e].day.maxtemp_c}<sup>o</sup>C</div>
-                     <small>${a[e].day.mintemp_c}<sup>o</sup></small>
-                        <div class="custom">${a[e].day.condition.text}</div>
-         </div> 
-        </div>`;
-    document.getElementById("forecast").innerHTML += t
 
+            <div><span>${weatherData.location.name}</span></div>
+
+            <div class="current_degree">
+                <h2 class='fs-1 m-0'>${weatherData.current.temp_c}<sup>o</sup>C</h2>
+                <div><img src="http:${weatherData.current.condition.icon}" alt=""></div>
+            </div>
+
+            <div>${weatherData.current.condition.text}</div>
+
+            <div>
+                <span><i class="fa-solid fa-umbrella"></i> ${weatherData.current.humidity}%</span>
+                <span><i class="fa-solid fa-wind"></i> ${weatherData.current.wind_kph}km/h</span>
+                <span><i class="fa-solid fa-compass"></i> ${weatherData.current.wind_dir}</span>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="col-lg-4">
+        <div class="items text-center shadow p-2">
+            <div><span>${dayName(weatherData.forecast.forecastday[1].date)}</span></div>
+            <div><img src="http:${weatherData.forecast.forecastday[1].day.condition.icon}" alt=""></div>
+            <div>
+                <p>${weatherData.forecast.forecastday[1].day.maxtemp_c}<sup>o</sup>C</p>
+                <span>${weatherData.forecast.forecastday[1].day.mintemp_c}<sup>o</sup></span>
+            </div>
+            <span>${weatherData.forecast.forecastday[1].day.condition.text}</span>
+        </div>
+    </div>
+    <div class="col-lg-4">
+        <div class="items text-center shadow p-2">
+            <div><span>${dayName(weatherData.forecast.forecastday[2].date)}</span></div>
+            <div><img src="http:${weatherData.forecast.forecastday[2].day.condition.icon}" alt=""></div>
+            <div>
+                <p>${weatherData.forecast.forecastday[2].day.maxtemp_c}<sup>o</sup>C</p>
+                <span>${weatherData.forecast.forecastday[2].day.mintemp_c}<sup>o</sup></span>
+            </div>
+            <span>${weatherData.forecast.forecastday[2].day.condition.text}</span>
+        </div>
+    </div>
+    `;
+    tRow.innerHTML = box;
 }
-search("cairo");
